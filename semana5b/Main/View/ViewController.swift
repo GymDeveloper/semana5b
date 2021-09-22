eaea62bd7f0268aa2164d21d0d517a49e8218bfe
linder3hs <linderhassinger00@gmail.com>
2021-09-22 13:35:19 -0500

    feat: (add table view) :zap:

--- semana5b/Main/View/ViewController.swift
+++ semana5b/Main/View/ViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
     // Ahora yo debo instanciar a mi clase ViewModel
     var viewModelHistoryPost = ViewModelHistoryPost()
+    
+    // vamos a crear un arreglo de prueba
+    let names: [String] = ["Pepito", "Juanito", "Sultano", "Lucas"]
+    let lastNames: [String] = ["Zapata", "Peres", "Macazana", "Chavez"]
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,8 @@ class ViewController: UIViewController {
         configureView()
         // lamar a bin
         bind()
+        // llamando al setUpTable
+        setUpTable()
     }
     
     func configureView() {
@@ -35,15 +43,44 @@ class ViewController: UIViewController {
         }
     }
     
+    func setUpTable() {
+        // aca voy a decir que el delegado mi table sea la propia vista
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
     func bind() {
         // Se encargue de refrescar data
         viewModelHistoryPost.refeshData = { [weak self] in
             DispatchQueue.main.async {
-                // aca voy a invoar a la funcion que se encargue de imprimir la informacion
+                // aca voy a invocar a la funcion que se encargue de imprimir la informacion
                 self?.printData()
+                // en esta parte vamos a invocar al metodo reloadData
+                self?.tableView.reloadData()
             }
         }
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // aca tiene que ir la cantidad de datos que tengo
+        return viewModelHistoryPost.dataArrayHistoryPosts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        let object = viewModelHistoryPost.dataArrayHistoryPosts
+        
+        cell.textLabel?.text = object[indexPath.row].title
+        cell.detailTextLabel?.text = object[indexPath.row].body
+        
+        return cell
+    }
+    
 
 }
 
